bc34bfe1d70739ead374cd4c44e873bf81771495
PianoRailways <piano@stellwerksim.ch>
2026-07-15 11:31:05 +0200

    Update data-mode/type

--- script.js
+++ script.js
@@ -436,7 +436,7 @@ function renderDepartures(departures) {
     tr.innerHTML = `
       <td class="col-time">${timeStr}<br><span class="delay-badge">${delayHtml}</span></td>
       <td class="col-line">
-        <div class="line-container" data-mode="${escapeHtml(getModeIcon(dep.mode))}"><span class="line">${iconHtml}${escapeHtml(dep.line)}</span></div>
+        <div class="line-container" data-mode="${canonicalMode(dep.mode)}"><span class="line">${iconHtml}${escapeHtml(dep.line)}</span></div>
         <div class="col-nr tripnr">${dep.tripNumber ? escapeHtml(dep.tripNumber) : ''}</div>
       </td>
       <td class="col-dest">${escapeHtml(dep.destination)}</td>
