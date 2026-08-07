6eaa30b5cc2ef7845de188d47ef90f372fcd6b4a
PianoRailways <piano@stellwerksim.ch>
2026-08-07 19:43:30 +0200

    Solothurn ergänzen

--- combinedTRAGIC.js
+++ combinedTRAGIC.js
@@ -3,4 +3,6 @@ window.combinedStations = {
     'Aarberg, Post/Bahnhof': ['Aarberg', 'Aarberg, Post/Bahnhof'],
     'Andermatt': ['Andermatt', 'Andermatt, Bahnhofplatz'],
     'Andermatt, Bahnhofplatz': ['Andermatt', 'Andermatt, Bahnhofplatz'],
+    'Solothurn, Hauptbahnhof': ['Solothurn', 'Solothurn, Hauptbahnhof'],
+    'Solothurn': ['Solothurn', 'Solothurn, Hauptbahnhof'],
 	};
