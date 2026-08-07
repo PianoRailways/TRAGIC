29fc758e0f6bb786a7d3fcbb2fecc4804c982564
PianoRailways <piano@stellwerksim.ch>
2026-08-07 11:03:51 +0200

    Andermatt ergänzen

--- combinedTRAGIC.js
+++ combinedTRAGIC.js
@@ -1,4 +1,6 @@
 window.combinedStations = {
     'Aarberg': ['Aarberg', 'Aarberg, Post/Bahnhof'],
     'Aarberg, Post/Bahnhof': ['Aarberg', 'Aarberg, Post/Bahnhof'],
+    'Andermatt': ['Andermatt', 'Andermatt, Bahnhofplatz'],
+    'Andermatt, Bahnhofplatz': ['Andermatt', 'Andermatt, Bahnhofplatz'],
 	};
