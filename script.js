0e6b31a01fd8b9ee1fa5d6f8f588f8ad195a098f
PianoRailways <piano@stellwerksim.ch>
2026-08-18 12:48:45 +0200

    revert IC, da sonst Liniennummer in der Schweiz abgeschnitten wird

--- script.js
+++ script.js
@@ -950,7 +950,6 @@ function normalizeLineDisplay(line) {
   if (upper.startsWith('ICE')) return 'ICE';
   if (upper.startsWith('ICD')) return 'ICD';
   if (upper.startsWith('ECD')) return 'ECD';
-  if (upper.startsWith('IC')) return 'ECD';
   if (upper.startsWith('L')) return 'L';
   if (upper.startsWith('FR')) return 'FR';
   if (upper.startsWith('EC')) return 'EC';
