08974c0b6fb1c69cd2e94cc07699182e7cf00e72
PianoRailways <piano@stellwerksim.ch>
2026-09-13 18:48:00 +0200

    Capitalize Heathrow Express

--- script/script-2.js
+++ script/script-2.js
@@ -470,7 +470,7 @@ function normalizeLineDisplay(line) {
   if (upper.startsWith('GATWICK EXPRESS')) return 'GX';
   if (upper.startsWith('ELIZABETH LINE')) return 'ELZ';
   if (upper.startsWith('HAMMERSMITH & CITY')) return 'H&C';
-  if (upper.startsWith('Heathrow Express')) return 'LHR';
+  if (upper.startsWith('HEATHROW EXPRESS')) return 'LHR';
   if (upper.startsWith('THAMESLINK')) return 'TL';
   
   return line.replace(/\s*\(\d+\)\s*$/g, '').trim();
