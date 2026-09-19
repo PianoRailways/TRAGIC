02276f64bd856184c8674b3f3df89f88bf387827
PianoRailways <piano@stellwerksim.ch>
2026-09-19 17:03:59 +0200

    Hochladen von Demo-Fahrten und des Editors

---
+++ cache/demo-fahrten.js
@@ -0,0 +1,35 @@
+{
+  "stations": [
+    {
+      "id": "custom_pinu_s_OFWF",
+      "name": "Otelfingen FEF",
+      "departures": [
+        {
+          "tripId": "custom_pinu_t_1",
+          "line": "FEXT",
+          "tripNumber": "1",
+          "destination": "Otelfingen Zentrum",
+          "scheduled": "2026-08-30T12:00:00+02:00",
+          "track": "F",
+          "mode": "RAIL",
+          "trip": {
+            "stops": [
+              {
+                "stopId": "custom_pinu_s_OFWFS",
+                "name": "Otelfingen FWF Süd",
+                "departureLive": "2026-08-30T12:00:00+02:00",
+                "track": "A",
+                "legIndex": null
+              },
+              {
+                "stopId": "custom_p_z",
+                "name": "Otelfingen Zentrum",
+                "legIndex": 0
+              }
+            ]
+          }
+        }
+      ]
+    }
+  ]
+}
