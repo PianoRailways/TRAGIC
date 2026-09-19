a49c2963ee583fc453b3876aa1c934223790d53c
PianoRailways <piano@stellwerksim.ch>
2026-09-19 17:24:54 +0200

    Demo Fahrt Update

--- cache/demo-fahrten.js
+++ cache/demo-fahrten.js
@@ -8,11 +8,16 @@
           "tripId": "custom_pinu_t_1",
           "line": "FEXT",
           "tripNumber": "1",
+          "agency": "FWF",
           "destination": "Otelfingen Zentrum",
           "scheduled": "2026-08-30T12:00:00+02:00",
           "track": "F",
           "mode": "RAIL",
           "trip": {
+            "agency": {
+              "id": "FWF1",
+              "name": "Freunde der Feldbahn Schweiz FWF"
+            },
             "stops": [
               {
                 "stopId": "custom_pinu_s_OFWFS",
